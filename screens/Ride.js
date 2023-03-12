dda0fc3b173d54c40831f4b69049ab011abf0ddd
Lucasmrqtt <marquettilu@gmail.com>
2023-03-11 23:12:33 -0300

    Projeto 70

--- screens/Ride.js
+++ screens/Ride.js
@@ -84,8 +84,7 @@ export default class RideScreen extends Component {
             <TouchableOpacity
               style={styles.scanbutton}
               //onPress=() => {this.getCameraPermissions()}
-              //onPress={() => this.getCameraPermissions()}
-              //onPress={() => this.getCameraPermissions}
+              onPress={() => this.getCameraPermissions()}
               //onPress=() => this.getCameraPermissions()
             >
               <Text style={styles.scanbuttonText}>Digitalizar</Text>
